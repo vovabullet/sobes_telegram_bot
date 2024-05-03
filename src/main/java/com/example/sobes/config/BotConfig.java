00cda4101be7f5cf406e00ad6c1ece01ef858a04
vovabullet <vovurlav@mail.ru>
2024-05-03 16:46:41 +0300

    add websocket

--- src/main/java/com/example/sobes/config/BotConfig.java
+++ src/main/java/com/example/sobes/config/BotConfig.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @Data // Автоматически создаёт конструкторы
@@ -13,6 +16,4 @@ public class BotConfig {
     String botName;
     @Value("${bot.token}")
     String token;
-
-
 }
